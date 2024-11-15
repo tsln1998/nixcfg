483ec101e1c2b80be66821b6ef3e5d4a245bd92e
Tsln <tsln1998@users.noreply.github.com>
2024-11-15 16:16:16 +0000

    feat: clean gnome

--- modules/gui/desktop/gnome/default.nix
+++ modules/gui/desktop/gnome/default.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ lib, pkgs,... }:
 {
   services.xserver.desktopManager.gnome = {
     enable = true;
   };
+
+  environment.gnome.excludePackages = [
+    pkgs.snapshot
+    pkgs.gnome-console
+    pkgs.gnome.epiphany
+    pkgs.gnome.yelp
+    pkgs.gnome.simple-scan
+    pkgs.gnome.gnome-contacts
+    pkgs.gnome.gnome-weather
+    pkgs.gnome.gnome-maps
+    pkgs.gnome.gnome-music
+  ];
 }
