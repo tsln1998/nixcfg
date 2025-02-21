3de9931832bfa791f64e9513591cf927fb03b983
Tsln <tsln1998@gmail.com>
2025-02-21 18:33:15 +0800

    feat: add gitconfig

--- home/tsln/common/features/dev/git.nix
+++ home/tsln/common/features/dev/git.nix
@@ -7,5 +7,25 @@
     enable = true;
     userName = "Tsln";
     userEmail = lib.mkDefault "tsln1998@gmail.com";
+    extraConfig = {
+      core = {
+        autocrlf = "input";
+        eol = "lf";
+      };
+      color = {
+        ui = "auto";
+      };
+      url = {
+        "ssh://git@ssh.github.com:443/" = {
+          insteadOf = "git@github.com:";
+        };
+        "ssh://git@altssh.gitlab.com:443/" = {
+          insteadOf = "git@gitlab.com:";
+        };
+        "ssh://git@codeup.aliyun.com/" = {
+          insteadOf = "https://codeup.aliyun.com/";
+        };
+      };
+    };
   };
 }
