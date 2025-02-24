f42a18f927a98942ed6600b1f0ed8ee3e926ea79
Tsln <tsln1998@users.noreply.github.com>
2025-02-24 17:23:29 +0800

    feat: combine files

--- users/tsln/home.nix
+++ users/tsln/home.nix
@@ -13,6 +13,24 @@ in
 {
   imports = [ (relative "<home-manager>") ];
 
+  # User configuration
+  users.mutableUsers = false;
+  users.users.tsln = {
+    shell = pkgs.zsh;
+    packages = [
+      pkgs.home-manager
+      pkgs.zsh
+    ];
+    extraGroups = builtins.filter (group: builtins.hasAttr group config.users.groups) [
+      "wheel"
+      "docker"
+    ];
+
+    isNormalUser = true;
+    hashedPasswordFile = config.age.secrets."users/tsln/passwd".path;
+  };
+
+  # Home Manager configuration
   home-manager.users.tsln = relative "home/tsln/${hostname}";
   home-manager.backupFileExtension = "hm-bak";
   home-manager.extraSpecialArgs = {
--- users/tsln/user.nix
+++ users/tsln/user.nix
@@ -4,16 +4,4 @@
   ...
 }:
 {
-  users.mutableUsers = false;
-  users.users.tsln = {
-    shell = pkgs.zsh;
-    packages = [ pkgs.home-manager ];
-    extraGroups = builtins.filter (group: builtins.hasAttr group config.users.groups) [
-      "wheel"
-      "docker"
-    ];
-
-    isNormalUser = true;
-    hashedPasswordFile = config.age.secrets."users/tsln/passwd".path;
-  };
 }
