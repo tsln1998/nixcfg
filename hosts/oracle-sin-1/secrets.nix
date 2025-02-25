7bcdcb7ef898933fcd9bdce03dcff23fbef3fac9
Tsln <tsln1998@users.noreply.github.com>
2025-02-25 22:14:17 +0800

    feat: enable xray

--- hosts/oracle-sin-1/secrets.nix
+++ hosts/oracle-sin-1/secrets.nix
@@ -1,10 +1,15 @@
-{ tools, ... }:
+{ config, tools, ... }:
 let
   inherit (tools) relative;
+  inherit (config.networking) hostName;
 in
 {
-  age.secrets."hosts/oracle-sin-1/caddyfile" = {
-    file = relative "secrets/hosts/oracle-sin-1/caddyfile.age";
+  age.secrets."hosts/${hostName}/caddyfile" = {
+    file = relative "secrets/hosts/${hostName}/caddyfile.age";
+    mode = "0644";
+  };
+  age.secrets."hosts/${hostName}/xray.json" = {
+    file = relative "secrets/hosts/${hostName}/xray.json.age";
     mode = "0644";
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -20,6 +20,7 @@ in
   "hosts/oracle-sin-1/ssh_host_ed25519_key.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
   "hosts/oracle-sin-1/ssh_host_ed25519_key.pub.age".publicKeys = keys.all;
   "hosts/oracle-sin-1/caddyfile.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
+  "hosts/oracle-sin-1/xray.json.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
 
   # User - tsln
   "users/tsln/passwd.age".publicKeys = keys.all;
