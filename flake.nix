87adc340c6f02b9095ba96e8ed7360f9c3dc2289
Tsln <tsln1998@users.noreply.github.com>
2025-03-02 23:07:07 +0800

    feat: add ustc mirrors

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   nixConfig = {
     substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
     ];
   };
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -5,6 +5,7 @@
   nix = {
     settings = {
       substituters = [
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
       ];
       experimental-features = [
