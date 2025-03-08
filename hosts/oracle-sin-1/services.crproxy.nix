fb4f225105cbe522dcb8e4deb25d99932886e3f8
Tsln <tsln1998@gmail.com>
2025-03-08 15:53:19 +0800

    feat: remove beszel

--- hosts/oracle-sin-1/services.crproxy.nix
+++ hosts/oracle-sin-1/services.crproxy.nix
@@ -1,7 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  crproxy = pkgs.additions.crproxy;
+  procps = pkgs.procps;
+
+  address = "127.0.0.1:18000";
+in
 {
-  services.crproxy = {
-    enable = true;
-    address = "127.0.0.1:18000";
+  systemd.services.crproxy = {
+    description = "CRProxy daemon";
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${crproxy}/bin/crproxy --address ${address}";
+      ExecStop = "${procps}/bin/pkill crproxy";
+      Restart = "on-failure";
+    };
   };
 }
