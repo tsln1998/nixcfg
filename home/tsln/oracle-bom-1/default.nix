c15759fb2fef9cdbcf13fe7257b7a11ea8ca93a5
Tsln <tsln1998@gmail.com>
2025-03-17 14:12:23 +0800

    chore: upgrade NixOS 24.11 20250316

--- home/tsln/oracle-bom-1/default.nix
+++ home/tsln/oracle-bom-1/default.nix
@@ -4,8 +4,6 @@
     map tools.relative [
       "home/tsln/common/global"
       "home/tsln/common/cli"
-      "home/tsln/common/apps/firefox.nix"
-      "home/tsln/common/apps/telegram.nix"
       "home/tsln/common/dev/nix.nix"
     ]
   );
