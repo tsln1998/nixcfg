37e072cb3cf757e11318a3fff6e3fa3b7df417c9
Tsln <tsln1998@gmail.com>
2025-04-25 20:15:42 +0800

    feat: ignore lid switch on ThinkPad X1 Carbon 2018

--- hosts/thinkpad-x1g6/default.nix
+++ hosts/thinkpad-x1g6/default.nix
@@ -4,10 +4,18 @@
     map tools.relative [
       "hosts/common/global"
       "hosts/common/services/openssh.nix"
+      #"hosts/common/desktop/sddm.nix"
+      #"hosts/common/desktop/plasma.nix"
       "users/tsln"
     ]
     ++ (tools.scan ./.)
   );
+
+  services.logind.lidSwitch = "ignore";
+  services.logind.suspendKey = "ignore";
+  services.logind.powerKey = "ignore";
+  services.logind.hibernateKey = "ignore";
+  services.logind.rebootKey = "ignore";
 
   system.stateVersion = "24.11";
 }
