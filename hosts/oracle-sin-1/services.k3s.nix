acd8f00f88a7a951e8b443e216dd48551e4425ff
Tsln <tsln1998@gmail.com>
2025-08-06 17:12:41 +0800

    feat: disable traefik

--- hosts/oracle-sin-1/services.k3s.nix
+++ hosts/oracle-sin-1/services.k3s.nix
@@ -61,6 +61,7 @@ in
   services.k3s.extraFlags = [
     "--write-kubeconfig-mode 644"
     "--node-external-ip 100.64.0.1"
+    "--disable traefik"
     "--disable local-storage"
     "--disable metrics-server"
   ];
