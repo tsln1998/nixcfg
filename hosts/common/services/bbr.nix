1e98da142a14d10b8df00c1bc9c63c3e7915c6e7
Tsln <tsln1998@gmail.com>
2025-10-09 18:30:17 +0800

    feat: use xanmod kernel

--- hosts/common/services/bbr.nix
+++ hosts/common/services/bbr.nix
@@ -2,12 +2,6 @@
 {
   boot.kernel.sysctl = {
     "net.core.default_qdisc" = "fq";
-    "net.ipv4.tcp_congestion_control" = "bbr";
-    "net.core.rmem_max" = 16777216;
-    "net.core.wmem_max" = 16777216;
-    "net.ipv4.tcp_rmem" = "4096 87380 16777216";
-    "net.ipv4.tcp_wmem" = "4096 87380 16777216";
-    "net.inet.udp.recvspace" = 16777216;
-    "kern.ipc.maxsockbuf" = 20971520;
+    "net.ipv4.tcp_congestion_control" = "fq_pie";
   };
 }
