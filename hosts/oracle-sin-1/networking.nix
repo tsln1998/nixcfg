ce82c167713400244bcc86dcac626cf74a6b3af1
Tsln <tsln1998@gmail.com>
2025-10-09 16:37:04 +0800

    feat: hysteria2 ports range

--- hosts/oracle-sin-1/networking.nix
+++ hosts/oracle-sin-1/networking.nix
@@ -3,4 +3,7 @@
   networking.hostName = "oracle-sin-1";
   networking.useDHCP = lib.mkDefault true;
   networking.usePredictableInterfaceNames = false;
+
+  networking.firewall.enable = true;
+  networking.nftables.enable = true;
 }
