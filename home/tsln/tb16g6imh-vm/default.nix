89310a1141cb51b65528e9892934de1023f4061e
Tsln <tsln1998@gmail.com>
2025-10-09 11:17:33 +0800

    feat: upgrade NixOS 20251006

--- home/tsln/tb16g6imh-vm/default.nix
+++ home/tsln/tb16g6imh-vm/default.nix
@@ -5,7 +5,6 @@
       "home/tsln/common/global"
       "home/tsln/common/cli"
       "home/tsln/common/dev/nix.nix"
-      "home/tsln/common/dev/go.nix"
       "home/tsln/common/i18n/locale.nix"
     ]
   );
