b93e96b5bd8f8989b6369247f29607f0389f16b0
Tsln <tsln1998@gmail.com>
2025-12-05 22:58:22 +0800

    refactor: reorganize age.identityPaths in agenix.nix and remove rclone service configuration

--- home/tsln/common/global/agenix.nix
+++ home/tsln/common/global/agenix.nix
@@ -1,7 +1,6 @@
 {
   tools,
   config,
-  lib,
   ...
 }:
 let
@@ -13,14 +12,14 @@ in
   age.identityPaths = [
     "${homeDirectory}/.ssh/id_rsa"
     "${homeDirectory}/.ssh/id_ed25519"
-    "/tmp/id_rsa_${username}"
-    "/tmp/id_ed25519_${username}"
     "/etc/ssh/ssh_host_ed25519_key"
     "/etc/ssh/ssh_host_rsa_key"
-    "/tmp/ssh_host_ed25519_key"
-    "/tmp/ssh_host_rsa_key"
     "/tmp/id_rsa"
     "/tmp/id_ed25519"
+    "/tmp/id_rsa_${username}"
+    "/tmp/id_ed25519_${username}"
+    "/tmp/ssh_host_ed25519_key"
+    "/tmp/ssh_host_rsa_key"
   ];
 
   age.secretsDir = homeDirectory + "/.agenix";
