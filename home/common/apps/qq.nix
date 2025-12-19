93aa55ff1c19a55ed33fbe9e023d0b8b1239795c
Tsln <tsln1998@gmail.com>
2025-12-19 22:36:03 +0800

    feat(apps): enable QQ with fcitx5 input method support
    Wrap QQ application with fcitx5 environment variables to enable
    proper Chinese input method support. Also set ELECTRON_OZONE_PLATFORM_HINT
    for better Wayland compatibility.

--- home/common/apps/qq.nix
+++ home/common/apps/qq.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 {
   home.packages = [
-    pkgs.qq
+    (pkgs.symlinkJoin {
+      name = "qq-with-fcitx5";
+      paths = [ pkgs.qq ];
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/qq \
+          --set QT_IM_MODULE fcitx \
+          --set XMODIFIERS "@im=fcitx" \
+          --set GTK_IM_MODULE fcitx \
+          --set ELECTRON_OZONE_PLATFORM_HINT auto
+      '';
+    })
   ];
 }
