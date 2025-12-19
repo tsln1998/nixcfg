56310f1ffe0419409980e173cabf8321eeac44ab
Tsln <tsln1998@gmail.com>
2025-12-19 22:35:39 +0800

    feat(apps): add dingtalk application support
    Add dingtalk application from NUR repository and enable it in
    tb16g6imh-vm configuration.

---
+++ home/common/apps/dingtalk.nix
@@ -0,0 +1,5 @@
+{pkgs,...}: {
+  home.packages = [
+    pkgs.nur.repos.xddxdd.dingtalk
+  ];
+}
--- modules/home/default.nix
+++ modules/home/default.nix
@@ -11,4 +11,7 @@
 
   nixpkgs.overlays = outputs.overlays;
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
 }
