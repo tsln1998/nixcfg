2c74530675790dd2fae04de61f03ad78bf4776d5
Tsln <tsln1998@gmail.com>
2025-12-29 12:03:55 +0800

    chore: intellij vmopts

--- home/common/apps/intellij.nix
+++ home/common/apps/intellij.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 let
   repo = pkgs.unstable;
   market = pkgs.jetbrains-plugins;
@@ -8,9 +8,20 @@ in
   home.packages = [
     (repo.jetbrains.plugins.addPlugins
       (release.override {
-        vmopts = ''
-          -Dawt.toolkit.name=WLToolkit
-        '';
+        vmopts = lib.strings.concatStringsSep "\n" [
+          # Increase Maximum Heap Size
+          "-Xms2048m"
+          "-Xmx4096m"
+          # Garbage Collection Tuning
+          "-XX:+UseParallelGC"
+          "-XX:ParallelGCThreads=4"
+          # Enable Wayland Support
+          "-Dawt.toolkit.name=WLToolkit"
+          # Network Tuning
+          "-Djava.net.preferIPv4Stack=true"
+          # Disable Updates
+          "-Dide.no.platform.update=true"
+        ];
       })
       (
         map (id: market.${release.pname}.${release.version}.${id}) [
