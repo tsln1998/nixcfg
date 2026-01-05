bd9611bef552fbc0cd380e0caa43f28607fe623e
Tsln <tsln1998@gmail.com>
2026-01-05 14:30:53 +0800

    refactor: remove unused nftables port forwarding for hysteria

--- hosts/oracle-sin-1/services.hysteria.nix
+++ hosts/oracle-sin-1/services.hysteria.nix
@@ -9,20 +9,6 @@ in
     configFile = secrets."hosts/${hostName}/hysteria.yaml".path;
   };
 
-  networking.nftables = {
-    tables = {
-      hysteria = {
-        family = "ip";
-        content = ''
-          chain prerouting {
-            type nat hook prerouting priority -100
-            iifname "eth0" udp dport 20000-50000 dnat to :443
-          }
-        '';
-      };
-    };
-  };
-
   networking.firewall = {
     allowedUDPPorts = [ 443 ];
   };
