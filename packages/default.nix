802bddf537f045a85569f8f513c1f8c5d268f883
Tsln <tsln1998@gmail.com>
2026-02-17 14:19:16 +0800

    feat: add binary packages for cliproxy and implement version-based selection
    - Add cliproxy-bin and cliproxy-plus-bin packages for prebuilt binaries
    - Support both x86_64-linux and aarch64-linux architectures
    - Implement automatic version comparison to select higher version package
    - Update cliproxy service to use version-aware package selection
    - Add new packages to default.nix exports

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,9 @@ pkgs: {
   crproxy = pkgs.callPackage ./crproxy/crproxy.nix { };
   caddy-l4 = pkgs.callPackage ./caddy/caddy-l4.nix { };
   cliproxy = pkgs.callPackage ./cliproxy/cliproxy.nix { };
+  cliproxy-bin = pkgs.callPackage ./cliproxy/cliproxy-bin.nix { };
   cliproxy-plus = pkgs.callPackage ./cliproxy/cliproxy-plus.nix { };
+  cliproxy-plus-bin = pkgs.callPackage ./cliproxy/cliproxy-plus-bin.nix { };
   cliproxy-management = pkgs.callPackage ./cliproxy/cliproxy-management.nix { };
   catppuccin-konsole = pkgs.callPackage ./catppuccin/konsole.nix { };
   vscode-extensions_vscode-buf = pkgs.callPackage ./vscode/vscode-buf.nix { };
