783aefbc10febe6674c51f054ce3246cb7ff8986
Tsln <tsln1998@gmail.com>
2026-03-02 10:54:16 +0800

    chore: update cliproxy packages to latest versions
    Update cliproxy-bin from 6.8.27 to 6.8.37, cliproxy-plus-bin from 6.8.27-0 to 6.8.37-0, and cliproxy-management from 1.7.0 to 1.7.1

--- packages/cliproxy/cliproxy-bin.nix
+++ packages/cliproxy/cliproxy-bin.nix
@@ -8,16 +8,16 @@ let
   owner = "router-for-me";
   repo = "CLIProxyAPI";
   pname = "cliproxy-bin";
-  version = "6.8.27";
+  version = "6.8.37";
 
   srcs = {
     x86_64-linux = fetchurl {
       url = "https://github.com/${owner}/${repo}/releases/download/v${version}/CLIProxyAPI_${version}_linux_amd64.tar.gz";
-      hash = "sha256-cSSLZL9Ps70D1x7TYf5nFDCNdFiz7dQv27cjnfBJAlE=";
+      hash = "sha256-KnwBX+lwVo+ksCFIhKluHm2xKaOuD3xStU64PKFO5zk=";
     };
     aarch64-linux = fetchurl {
       url = "https://github.com/${owner}/${repo}/releases/download/v${version}/CLIProxyAPI_${version}_linux_arm64.tar.gz";
-      hash = "sha256-8WkrBlGRJ61YtLuj0LHXOFCYYShCx/24AN8aD/5a49A=";
+      hash = "sha256-PsuSMrON85sVcMGQKJWsENgG3cyL85sLmidljUohbPk=";
     };
   };
 
