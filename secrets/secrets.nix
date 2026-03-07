00c1d702b6d4c335975beeb3a3f354b6fd9fe596
Tsln <tsln1998@gmail.com>
2026-03-07 23:09:19 +0800

    chore: remove Hysteria service from oracle-sin hosts
    - Remove Hysteria service configuration files from oracle-sin-1 and oracle-sin-2
    - Delete Hysteria YAML secrets for both hosts
    - Update Caddyfile secrets for oracle-sin-1 and oracle-sin-2
    - Update mihomo configuration secret for tb16g6imh
    - Clean up secrets.nix entries

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -15,18 +15,16 @@ in
   # Oracle Cloud Singapore 1
   "hosts/oracle-sin-1/ssh_host_ed25519_key.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
   "hosts/oracle-sin-1/ssh_host_ed25519_key.pub.age".publicKeys = keys.all;
+  "hosts/oracle-sin-1/rclone.toml.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
   "hosts/oracle-sin-1/hub.env.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
   "hosts/oracle-sin-1/cpa.env.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
   "hosts/oracle-sin-1/xray.json.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
-  "hosts/oracle-sin-1/hysteria.yaml.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
   "hosts/oracle-sin-1/shadowsocks.json.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
-  "hosts/oracle-sin-1/rclone.toml.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
   "hosts/oracle-sin-1/caddyfile.age".publicKeys = hosts.oracle-sin-1 ++ users.tsln;
 
   # Oracle Cloud Singapore 2
   "hosts/oracle-sin-2/ssh_host_ed25519_key.age".publicKeys = hosts.oracle-sin-2 ++ users.tsln;
   "hosts/oracle-sin-2/ssh_host_ed25519_key.pub.age".publicKeys = keys.all;
-  "hosts/oracle-sin-2/hysteria.yaml.age".publicKeys = hosts.oracle-sin-2 ++ users.tsln;
   "hosts/oracle-sin-2/xray.json.age".publicKeys = hosts.oracle-sin-2 ++ users.tsln;
   "hosts/oracle-sin-2/shadowsocks.json.age".publicKeys = hosts.oracle-sin-2 ++ users.tsln;
   "hosts/oracle-sin-2/caddyfile.age".publicKeys = hosts.oracle-sin-2 ++ users.tsln;
