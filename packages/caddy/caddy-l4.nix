feafcdf3d2b8a002e6eec48d04d3bf56126f8af9
Tsln <tsln1998@gmail.com>
2026-03-24 15:01:00 +0800

    🐛 bugfix: 更新 caddy-l4 构建哈希
    同步 caddy-l4 插件源码的固定输出哈希。
    修复 caddy.withPlugins 因哈希漂移导致的构建失败。

--- packages/caddy/caddy-l4.nix
+++ packages/caddy/caddy-l4.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 pkgs.caddy.withPlugins {
   plugins = [ "github.com/mholt/caddy-l4@v0.0.0-20260104223739-97fa8c1b6618" ];
-  hash = "sha256-tU1NDghF8lfnirMIhIrC08dBLtpds8Clpf0D2MydDh0=";
+  hash = "sha256-+8gihZALxygPkb5nWbsVBDqcS2G4yK/E6qOpbRBRLEk=";
 }
