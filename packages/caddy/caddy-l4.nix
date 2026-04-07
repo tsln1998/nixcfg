a09dbf72c249a4faa160ced8e0ac6e5c59d8ed08
Tsln <tsln1998@gmail.com>
2026-04-07 23:17:37 +0800

    🐛 bugfix: 修复 caddy-l4 打包配置 [backports-25.11]

--- packages/caddy/caddy-l4.nix
+++ packages/caddy/caddy-l4.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
-pkgs.caddy.withPlugins {
+let
+  locked =
+    import
+      (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/4533d9293756b63904b7238acb84ac8fe4c8c2c4.tar.gz";
+        sha256 = "1n33324pfdd57mk25kp6qnl78d3s53grmg0m2lsj0gknph8v9d5m";
+      })
+      {
+        system = pkgs.stdenv.hostPlatform.system;
+        config = pkgs.config;
+      };
+in
+locked.caddy.withPlugins {
   plugins = [ "github.com/mholt/caddy-l4@v0.0.0-20260104223739-97fa8c1b6618" ];
-  hash = "sha256-+8gihZALxygPkb5nWbsVBDqcS2G4yK/E6qOpbRBRLEk=";
+  hash = "sha256-9tFRk+ULLh99eSPiNtiusH9yqcIJkVuJOEaeS43s8Tc=";
 }
