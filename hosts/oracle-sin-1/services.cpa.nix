daadd74b54c142fc9e29229a94ca714be1aa6e67
Tsln <tsln1998@gmail.com>
2026-04-08 18:27:21 +0800

    🎉 feat: upgrade cliproxyapi & sub2api [backports-25.11]

--- hosts/oracle-sin-1/services.cpa.nix
+++ hosts/oracle-sin-1/services.cpa.nix
@@ -13,7 +13,7 @@ in
 {
   # CLIProxy service configuration
   virtualisation.oci-containers.containers.cpa = {
-    image = "eceasy/cli-proxy-api:v6.9.15";
+    image = "eceasy/cli-proxy-api:v6.9.16";
 
     environment = {
       PGSTORE_DSN = "postgresql://${db}@127.0.0.1:${builtins.toString config.services.postgresql.settings.port}/${db}";
