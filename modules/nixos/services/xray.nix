1f4b1fa4c772a5bfb253ba1a8ed3abff9a16d9cc
Tsln <tsln1998@gmail.com>
2026-04-19 23:56:46 +0800

    🎉 feat: 优化 Nginx 性能参数、开启 Xray 背压

--- modules/nixos/services/xray.nix
+++ modules/nixos/services/xray.nix
@@ -1,6 +1,11 @@
 _: {
-  systemd.services.xray.serviceConfig = {
-    RuntimeDirectory = "xray";
-    RuntimeDirectoryMode = "0755";
+  systemd.services.xray = {
+    serviceConfig = {
+      RuntimeDirectory = "xray";
+      RuntimeDirectoryMode = "0755";
+    };
+    environment = {
+      XRAY_RAY_BUFFER_SIZE = "4";
+    };
   };
 }
