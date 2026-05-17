3bff93835918be303bfa843b96e2df746413b08f
Tsln <tsln1998@gmail.com>
2026-05-17 10:20:12 +0800

    🚧 chore: 删除 Claude Code 配置

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -42,7 +42,6 @@ in
   # User - tsln
   "users/tsln/id_ed25519.age".publicKeys = users.tsln;
   "users/tsln/id_ed25519.pub.age".publicKeys = keys.all;
-  "users/tsln/config/profile.d/claude.sh.age".publicKeys = users.tsln;
   "users/tsln/kube/config.age".publicKeys = users.tsln;
   "users/tsln/codex/config.toml.age".publicKeys = users.tsln;
   "users/tsln/codex/auth.json.age".publicKeys = users.tsln;
