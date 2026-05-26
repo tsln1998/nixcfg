6173fdfed3c529c23478cdbd18b39bd17765151d
Tsln <tsln1998@gmail.com>
2026-05-26 15:59:08 +0000

    🎉 feat: tailscale persist

--- hosts/oracle-sin-2/immutable.nix
+++ hosts/oracle-sin-2/immutable.nix
@@ -27,6 +27,10 @@ _: {
         mode = "0755";
       }
       {
+        directory = "/var/lib/tailscale";
+        mode = "0755";
+      }
+      {
         directory = "/var/lib/systemd/coredump";
         mode = "0755";
       }
