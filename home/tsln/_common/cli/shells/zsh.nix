cb22771ca4a33941b87ebc43d87b029f1ca99eed
Tsln <tsln1998@gmail.com>
2026-06-11 12:06:10 +0800

    🚧 chore: 调整 zsh 默认交互配置
    关闭 zsh 的 autocd 行为，显式设置默认键位为 emacs，并移除重复的初始化选项与自定义按键绑定。

--- home/tsln/_common/cli/shells/zsh.nix
+++ home/tsln/_common/cli/shells/zsh.nix
@@ -1,16 +1,11 @@
 _: {
   programs.zsh = {
     enable = true;
-    autocd = true;
+    autocd = false;
+    defaultKeymap = "emacs";
 
     initContent = ''
-      # Opts
       setopt local_options nullglob
-      unsetopt AUTO_CD
-
-      # Bindings
-      bindkey '^[[1;5C' forward-word  # Ctrl + right
-      bindkey '^[[1;5D' backward-word # Ctrl + left
 
       # Load local profile
       for profile in ~/.config/profile.d/*.sh; do
