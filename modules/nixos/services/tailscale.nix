076e9f8a1cf4c6a338e8c2f4388c88af76f8cf16
Tsln <tsln1998@gmail.com>
2026-06-21 00:07:59 +0800

    🎉 feat: add tailscale exit node

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -1,29 +1,45 @@
 { config, lib, ... }:
 let
-  inherit (config.services.tailscale) enable openFirewall relay;
+  inherit (config.services.tailscale) enable openFirewall relay exit;
   inherit (config.networking) nftables;
 in
 {
-  options.services.tailscale.relay = {
-    enable = lib.mkEnableOption "Enable tailscale peer relay";
-    port = lib.mkOption {
-      type = lib.types.port;
-      default = 40000;
-      description = "tailscale peer relay port";
+  options.services.tailscale = {
+    exit = {
+      enable = lib.mkEnableOption "Enable tailscale exit node";
+    };
+    relay = {
+      enable = lib.mkEnableOption "Enable tailscale peer relay";
+      port = lib.mkOption {
+        type = lib.types.port;
+        default = 40000;
+        description = "tailscale peer relay port";
+      };
     };
   };
 
   config = lib.mkIf enable {
-    services.tailscale.extraSetFlags = lib.optionals relay.enable [
+    # Set flags
+    services.tailscale.extraSetFlags = (lib.optionals relay.enable [
       "--relay-server-port=${toString relay.port}"
-    ];
+    ]) ++ (lib.optionals exit.enable [
+      "--advertise-exit-node"
+    ]);
 
-    systemd.services.tailscaled.environment = lib.mkIf nftables.enable {
-      TS_DEBUG_FIREWALL_MODE = "nftables";
+    # Exit node
+    boot.kernel.sysctl = lib.optionalAttrs exit.enable {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
     };
 
+    # Peer relay
     networking.firewall.allowedUDPPorts = lib.optionals (relay.enable && openFirewall) [
       relay.port
     ];
+
+    # Nftables
+    systemd.services.tailscaled.environment = lib.optionalAttrs nftables.enable {
+      TS_DEBUG_FIREWALL_MODE = "nftables";
+    };
   };
 }
