cadc53efc45d5322d4edc4041f214fba3064adcf
Tsln <tsln1998@gmail.com>
2026-07-21 09:54:47 +0800

    🎉 feat: 配置 Zellij 并更新系统依赖
    新增 Zellij 启用配置、快捷别名与分离快捷键。
    更新 nixpkgs unstable 锁定版本及相关 age 加密配置。

---
+++ home/tsln/_common/cli/shells/zellij.nix
@@ -0,0 +1,22 @@
+{...}:
+{
+  programs.zellij = {
+    enable = true;
+    extraConfig = ''
+      show_startup_tips false
+
+      keybinds {
+          shared_except "locked" {
+              bind "Ctrl \\" { Detach; }
+          }
+          locked {
+              bind "Ctrl \\" { Detach; }
+          }
+      }
+    '';
+  };
+
+  home.shellAliases = {
+    z = "zellij";
+  };
+}
