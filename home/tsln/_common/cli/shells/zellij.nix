51c50d0fa1cc62de42d8825a2c9b9576e9bad43f
Tsln <tsln1998@gmail.com>
2026-07-23 19:19:40 +0800

    🚧 chore: 移除 zellij 默认分离快捷键配置

--- home/tsln/_common/cli/shells/zellij.nix
+++ home/tsln/_common/cli/shells/zellij.nix
@@ -4,15 +4,6 @@
     enable = true;
     extraConfig = ''
       show_startup_tips false
-
-      keybinds {
-          shared_except "locked" {
-              bind "Ctrl \\" { Detach; }
-          }
-          locked {
-              bind "Ctrl \\" { Detach; }
-          }
-      }
     '';
   };
 
