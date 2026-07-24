1086faf9752cbcefac5fc74a2d08aa62a9de0245
Tsln <tsln1998@gmail.com>
2026-07-24 16:22:30 +0800

    🚧 chore: 同步 DataGrip 与 unstable overlay 配置

--- overlays/repos/unstable-2511.nix
+++ overlays/repos/unstable-2511.nix
@@ -1,10 +1,10 @@
 { inputs, ... }: _: prev: {
-  repos = (prev.repos or {}) // {
+  repos = (prev.repos or { }) // {
     unstable-2511 = import inputs.unstable-2511 {
       system = prev.stdenv.hostPlatform.system;
       config = {
         allowUnfree = prev.config.allowUnfree;
-        allowUnfreePackages = prev.config.allowUnfreePackages;
+        allowUnfreePredicate = pkg: builtins.elem (prev.lib.getName pkg) prev.config.allowUnfreePackages;
         permittedInsecurePackages = prev.config.permittedInsecurePackages;
       };
     };
