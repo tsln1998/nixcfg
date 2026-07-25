d7d44d611627cfec04a3a7e7b310004c5ddd9877
Tsln <tsln1998@gmail.com>
2026-07-25 09:48:34 +0800

    🚧 chore: 为 minipc 增加 kmscon 与 plymouth 配置

--- hosts/minipc/default.nix
+++ hosts/minipc/default.nix
@@ -6,6 +6,8 @@
       "hosts/_common/i18n"
       "hosts/_common/gui/display/sddm.nix"
       "hosts/_common/gui/desktop/plasma.nix"
+      "hosts/_common/kernel/plymouth.nix"
+      "hosts/_common/themes"
       "users/tsln"
     ]
     ++ (tools.scan ./.);
