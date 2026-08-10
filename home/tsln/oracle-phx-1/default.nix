785f84882208650cb42185b91fb9ba41e494031f
Tsln <tsln1998@gmail.com>
2026-08-10 10:28:46 +0800

    🚧 chore: 统一 Oracle 配置中的 Git 模块路径
    使用新的 home/tsln/_common_/dev/programs/git.nix 路径取代已废弃的 languages/git 与 dotfiles/git 引用，保持两套主机配置与新模块结构一致。

--- home/tsln/oracle-phx-1/default.nix
+++ home/tsln/oracle-phx-1/default.nix
@@ -4,8 +4,7 @@
     (map tools.relative [
       "home/tsln/_common_/base"
       "home/tsln/_common_/cli/shells"
-      "home/tsln/_common_/dev/languages/git.nix"
-      "home/tsln/_common_/dev/languages/dotfiles/git.nix"
+      "home/tsln/_common_/dev/programs/git.nix"
     ])
     ++ (tools.scan ./.);
 }
