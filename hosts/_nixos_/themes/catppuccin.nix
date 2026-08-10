9d7bdc0aeb680d3e36501a7aeff2915288db531f
Tsln <tsln1998@gmail.com>
2026-08-10 10:35:27 +0800

    🚧 chore: 调整 Catppuccin 主题配置
    关闭公共主题的 autoEnable 自动启用逻辑，并在 NixOS 主题配置中显式启用 Grub、Plymouth 与 SDDM。

--- hosts/_nixos_/themes/catppuccin.nix
+++ hosts/_nixos_/themes/catppuccin.nix
@@ -1,8 +1,11 @@
 { lib, ... }:
 {
   catppuccin = {
-    sddm = {
-      flavor = lib.mkDefault "latte";
-    };
+    grub.enable = lib.mkDefault true;
+
+    plymouth.enable = lib.mkDefault true;
+
+    sddm.enable = lib.mkDefault true;
+    sddm.flavor = lib.mkDefault "latte";
   };
 }
