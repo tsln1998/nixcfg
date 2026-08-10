ec867ee5edbbb48d09ad8adffe049224ae283bf8
Tsln <tsln1998@gmail.com>
2026-08-10 10:05:34 +0800

    🔨 refactor: 重构配置为统一基座复用

--- home/tsln/_nixos_/gui/apps/vscode.nix
+++ home/tsln/_nixos_/gui/apps/vscode.nix
@@ -1,6 +1,7 @@
 {
-  config,
+  lib,
   pkgs,
+  _vsc_base_profile,
   ...
 }:
 let
@@ -8,61 +9,57 @@ let
   market_1 = pkgs.repos.unstable.vscode-extensions;
   market_2 = pkgs.repos.vscode.vscode-marketplace-release;
 
-  inherit (config.programs.${pkg.pname}) profiles;
-  inherit (config.programs.${pkg.pname}.profiles) default;
-in
-{
-  # 生成 Profiles
-  programs.${pkg.pname}.profiles.Go = default // {
-    extensions = default.extensions ++ [
+  langExtensions = rec {
+    Go = [
       market_1.golang.go
       market_2.bufbuild.vscode-buf
     ];
 
-    userSettings = default.userSettings // {
-      "go.showWelcome" = false;
-      "go.diagnostic.vulncheck" = "Off";
-    };
-  };
-
-  programs.${pkg.pname}.profiles.Rust = default // {
-    extensions = default.extensions ++ [
+    Rust = [
       market_1.rust-lang.rust-analyzer
       market_1.vadimcn.vscode-lldb
     ];
-  };
 
-  programs.${pkg.pname}.profiles.Python = default // {
-    extensions = default.extensions ++ [
+    Python = [
       market_1.ms-python.python
       market_1.ms-python.debugpy
       market_1.ms-python.isort
       market_1.ms-python.vscode-pylance
       market_2.ms-python.autopep8
     ];
-  };
 
-  programs.${pkg.pname}.profiles.CXX = default // {
-    extensions = default.extensions ++ [
+    Cxx = [
       market_1.ms-vscode.cpptools
       market_1.ms-vscode.cmake-tools
       market_1.vadimcn.vscode-lldb
     ];
+
+    All = Go ++ Rust ++ Python ++ Cxx;
   };
 
-  programs.${pkg.pname}.profiles.All = default // {
-    extensions =
-      default.extensions
-      ++ profiles.Go.extensions
-      ++ profiles.Rust.extensions
-      ++ profiles.Python.extensions
-      ++ profiles.CXX.extensions;
-    userSettings =
-      default.userSettings
-      // profiles.Go.userSettings
-      // profiles.Rust.userSettings
-      // profiles.Python.userSettings
-      // profiles.CXX.userSettings;
+  langSettings = rec {
+    Go = {
+      "go.showWelcome" = false;
+      "go.diagnostic.vulncheck" = "Off";
+    };
+
+    All = Go;
   };
+
+  names = lib.attrNames (langExtensions // langSettings);
+in
+{
+  # 生成 Profiles
+  programs.${pkg.pname}.profiles = lib.genAttrs names (
+    name:
+    _vsc_base_profile
+    // {
+      extensions = _vsc_base_profile.extensions ++ (langExtensions.${name} or [ ]);
+      userSettings = _vsc_base_profile.userSettings // {
+        "window.titleBarStyle" = lib.mkForce "native";
+        "window.menuStyle" = lib.mkForce "custom";
+      } // (langSettings.${name} or { });
+    }
+  );
 
 }
