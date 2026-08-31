9a055b75cfd94b3ee5d3d9e5cc0530e44eda36de
Tsln <tsln1998@gmail.com>
2026-08-31 17:54:52 +0800

    🚧 chore: 调整 tailscale 服务配置
    新增 magic-dns 开关与 systemd-resolved 兼容参数。

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -1,24 +1,49 @@
 { config, lib, ... }:
 let
+  inherit (config.services) resolved;
+  inherit (config.networking) nftables networkmanager;
+
+  # extract base setting
   inherit (config.services.tailscale)
     enable
     openFirewall
+    ;
+
+  # extract features setting
+  inherit (config.services.tailscale)
     relay
     exit
+    magic-dns
     ;
-  inherit (config.networking) nftables;
 in
 {
   options.services.tailscale = {
     exit = {
-      enable = lib.mkEnableOption "Enable tailscale exit node";
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable tailscale exit node";
+      };
     };
+
     relay = {
-      enable = lib.mkEnableOption "Enable tailscale peer relay";
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable tailscale peer relay";
+      };
       port = lib.mkOption {
         type = lib.types.port;
         default = 40000;
         description = "tailscale peer relay port";
+      };
+    };
+
+    magic-dns = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable tailscale magic-dns";
       };
     };
   };
@@ -31,20 +56,31 @@ in
       ])
       ++ (lib.optionals exit.enable [
         "--advertise-exit-node"
-      ]);
-
-    # Exit node
-    boot.kernel.sysctl = lib.optionalAttrs exit.enable {
-      "net.ipv4.ip_forward" = 1;
-      "net.ipv6.conf.all.forwarding" = 1;
-    };
+      ])
+      ++ [
+        "--accept-dns=${lib.boolToString magic-dns.enable}"
+      ];
 
     # Peer relay
     networking.firewall.allowedUDPPorts = lib.optionals (relay.enable && openFirewall) [
       relay.port
     ];
 
+    # Exit node
+    # See: https://tailscale.com/docs/features/subnet-routers#enable-ip-forwarding
+    boot.kernel.sysctl = lib.optionalAttrs exit.enable {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
+
+    # MagicDNS
+    # See: https://tailscale.com/docs/reference/linux-dns#networkmanager--systemd-resolved
+    networking.networkmanager = lib.optionalAttrs (magic-dns.enable && resolved.enable && networkmanager.enable) {
+      dns = lib.mkForce "systemd-resolved";
+    };
+
     # Nftables
+    # See: https://tailscale.com/docs/features/firewall-mode
     systemd.services.tailscaled.environment = lib.optionalAttrs nftables.enable {
       TS_DEBUG_FIREWALL_MODE = "nftables";
     };
