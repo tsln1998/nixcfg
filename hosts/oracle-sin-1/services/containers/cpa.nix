184092203d7b02abcf474f302cf9f7232551bf09
Tsln <tsln1998@gmail.com>
2026-09-10 15:20:18 +0800

    🎉 feat: 更新 CLIProxyAPI 配置

--- hosts/oracle-sin-1/services/containers/cpa.nix
+++ hosts/oracle-sin-1/services/containers/cpa.nix
@@ -17,7 +17,7 @@ in
 
   # CLIProxy service configuration
   virtualisation.oci-containers.containers.${name} = {
-    image = "eceasy/cli-proxy-api:v7.2.147";
+    image = "eceasy/cli-proxy-api:v7.2.156";
 
     serviceName = name;
 
