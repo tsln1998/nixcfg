de65d39b195198bb29a87ea4a86e50288ea54c5e
Tsln <tsln1998@gmail.com>
2026-09-15 18:49:14 +0800

    🚧 chore: 移除 pen 包并升级 waku 至 0.1.19
    - 删除 packages/pen 包定义及 home 模块中的引用（pen.nix、packages/default.nix、modules/home/default.nix）
    - 更新 packages/waku/hashes.json 至 v0.1.19，同步 aarch64 与 x86_64 的 hash 和下载地址
    - 更新 pi agent models.json 密钥

--- packages/default.nix
+++ packages/default.nix
@@ -3,5 +3,4 @@ pkgs:
   catppuccin-konsole = pkgs.callPackage ./catppuccin/konsole.nix { };
   codex = pkgs.callPackage ./codex { };
   waku = pkgs.callPackage ./waku { };
-  pen = pkgs.callPackage ./pen { };
 }
