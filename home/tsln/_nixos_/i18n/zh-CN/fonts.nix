4a83ea6c79885b314e91348777353c8befaf2ca2
Tsln <tsln1998@gmail.com>
2026-09-18 14:51:40 +0800

    🗑️ revert: 移除 waku 相关配置
    删除 waku 应用模块与本地包定义，并将原本附带的 ibm-plex 字体移到 i18n 字体模块。

--- home/tsln/_nixos_/i18n/zh-CN/fonts.nix
+++ home/tsln/_nixos_/i18n/zh-CN/fonts.nix
@@ -29,6 +29,7 @@
   };
 
   home.packages = with pkgs; [
+    ibm-plex
     monaspace
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
--- packages/default.nix
+++ packages/default.nix
@@ -2,5 +2,4 @@ pkgs:
 {
   catppuccin-konsole = pkgs.callPackage ./catppuccin/konsole.nix { };
   codex = pkgs.callPackage ./codex { };
-  waku = pkgs.callPackage ./waku { };
 }
